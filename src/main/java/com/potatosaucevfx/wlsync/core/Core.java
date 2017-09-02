d6c698a12e2dcf8576e80ed9ffac0f9d2562bc78
RJ-Fynydd <richard.nader@fynydd.com>
2017-09-01 20:12:13 -0400

    Updated to 1.11.2!

--- src/main/java/com/potatosaucevfx/wlsync/core/Core.java
+++ src/main/java/com/potatosaucevfx/wlsync/core/Core.java
@@ -24,7 +24,7 @@ import java.io.File;
 public class Core {
 
     public static final String MODID = "wlsync";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "2.0";
     public static final String LOG_PREFIX = "[Whitelist Sync " + VERSION + "] ";
     public static String SERVER_FILEPATH = "";
     public static Configuration config;
